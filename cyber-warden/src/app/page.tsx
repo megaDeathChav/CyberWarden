64730fe21ea51dd9e4397b774de47e7d15547dc1
PR1ZR4K1 <jaylonignacio@gmail.com>
2023-09-19 10:30:01 -0700

    Basic Routing and Login/Dashboard skeleton

--- cyber-warden/src/app/page.tsx
+++ cyber-warden/src/app/page.tsx
@@ -1,34 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import login from './login/page';
+import { DataGrid } from '@mui/x-data-grid';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <div className="max-w-md w-full mx-auto mt-4 bg-white rounded-2xl p-8 border border-gray-300">
-        {/* Stylized text */}
-        <div className="text-center mb-4">
-          <p className="text-3xl font-extrabold text-black">Login </p>
-          <p className="text-sm font-bold text-gray-600">to Cyberwarden</p>
-        </div>
-        <form action="" className="space-y-6">
-          <div>
-            <label htmlFor="email" className="text-sm font-bold text-black block">Email</label>
-            <input type="text" id="email" className="w-full p-2 border border-gray-300 rounded mt-1" />
-          </div>
-          <div>
-            <label htmlFor="password" className="text-sm font-bold text-black block">Password</label>
-            <input type="password" id="password" className="w-full p-2 border border-gray-300 rounded mt-1" />
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input type="checkbox" id="rememberMe" className="h-4 w-4 text-blue-300 rounded" />
-              <label htmlFor="rememberMe" className="ml-2 text-sm text-gray-500">Remember me</label>
-            </div>
-            <a href="" className="font-medium text-sm text-gray-500">Forgot Password?</a>
-          </div>
-          <div>
-            <button className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm">Proceed</button>
-          </div>
-        </form>
+    <main className="flex flex-col min-h-screen items-center justify-center">
+      <div>
+        Here is the dashboard
+      </div>
+
+      <div className='mt-4'>
+        <Link href='/login'>Login Here </Link>
       </div>
     </main>
   )
