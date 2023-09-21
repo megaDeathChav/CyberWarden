c498f76f529dd05bb93cca912b44de314177b761
PR1Z4R4K1 <jaylonignacio@gmail.com>
2023-09-21 14:32:09 -0700

    added basic sidebar :D

--- cyber-warden/src/app/page.tsx
+++ cyber-warden/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from 'next/image';
 import Link from 'next/link';
 import login from './login/page';
@@ -6,6 +8,21 @@ import RootLayout from './layout';
 import CustomSidebar from '../components/CustomSidebar';
 import LoginIcon from '@mui/icons-material/Login';
 
+import { useState, useEffect } from "react";
+import { Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { Sidebar } from "../components/Sidebar";
+// import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import SidebarItem from "../components/SidebarItem";
+// icon import
+import { Diamond } from '../icons/Diamond';
+import { BarChart } from '../icons/BarChart';
+import { Global } from '../icons/Global';
+import { InkBottle } from '../icons/InkBottle';
+import { Book } from '../icons/Book';
+import { Calendar } from '../icons/Calendar';
+import { ShoppingCart } from '../icons/ShoppingCart';
+import { Service } from '../icons/Service';
 
 
 export default function Home() {
@@ -17,12 +34,132 @@ export default function Home() {
       icon: <LoginIcon />,
     },
   ];
+
+  // Type for menuItems
+  interface MenuItemType {
+    title: string;
+    to: string;
+    icon: JSX.Element;
+  }
+
+  interface SidebarProps {
+    menuItems: MenuItemType[];
+    currentPage: string;
+  }
+
+  const [collapsed, setCollapsed] = useState(false);
+  const [toggled, setToggled] = useState(false);
+  const [broken, setBroken] = useState(false);
+  const [rtl, setRtl] = useState(false);
+  const [hasImage, setHasImage] = useState(false);
+
+  // handle on RTL change event
+  const handleRTLChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRtl(e.target.checked);
+  };
+
+  // handle on image change event
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHasImage(e.target.checked);
+  };
+
+  
+
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  // currentPage is a string that is used to check which page the user is currently viewing
+  // depending on that the corresponding icon will be blue
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+    }, []);
   
   return (
-  <>
-    <CustomSidebar menuItems={menuItems} currentPage="Home" />
+  <div style={{ display: 'flex', height: '100vh' }}>
+    <div style={{ width: '250px' }}>
+      <Sidebar
+        collapsed={collapsed}
+        toggled={toggled}
+        onBackdropClick={() => setToggled(false)}
+        onBreakPoint={setBroken}
+        image="https://user-images.githubusercontent.com/25878302/144499035-2911184c-76d3-4611-86e7-bc4e8ff84ff5.jpg"
+        rtl={rtl}
+        breakPoint="md"
+        backgroundColor={'#ffffff'}
+        rootStyles={{
+          color: '#607489',
+        }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+          <div style={{ flex: 1, marginBottom: '32px' }}>
+            <div style={{ padding: '0 24px', marginBottom: '8px' }}>
+              <Typography
+                variant="body2"
+                fontWeight={600}
+                style={{ opacity: collapsed ? 0 : 0.7, letterSpacing: '0.5px' }}
+              >
+                General
+              </Typography>
+            </div>
+            <Menu>
+              <SubMenu
+                label="Charts"
+                icon={<BarChart />}
+              >
+                <MenuItem> Pie charts</MenuItem>
+                <MenuItem> Line charts</MenuItem>
+                <MenuItem> Bar charts</MenuItem>
+              </SubMenu>
+              <SubMenu label="Maps" icon={<Global />}>
+                <MenuItem> Google maps</MenuItem>
+                <MenuItem> Open street maps</MenuItem>
+              </SubMenu>
+              <SubMenu label="Theme" icon={<InkBottle />}>
+                <MenuItem> Dark</MenuItem>
+                <MenuItem> Light</MenuItem>
+              </SubMenu>
+              <SubMenu label="Components" icon={<Diamond />}>
+                <MenuItem> Grid</MenuItem>
+                <MenuItem> Layout</MenuItem>
+                <SubMenu label="Forms">
+                  <MenuItem> Input</MenuItem>
+                  <MenuItem> Select</MenuItem>
+                  <SubMenu label="More">
+                    <MenuItem> CheckBox</MenuItem>
+                    <MenuItem> Radio</MenuItem>
+                  </SubMenu>
+                </SubMenu>
+              </SubMenu>
+              <SubMenu label="E-commerce" icon={<ShoppingCart />}>
+                <MenuItem> Product</MenuItem>
+                <MenuItem> Orders</MenuItem>
+                <MenuItem> Credit card</MenuItem>
+              </SubMenu>
+            </Menu>
 
+            <div style={{ padding: '0 24px', marginBottom: '8px', marginTop: '32px' }}>
+              <Typography
+                variant="body2"
+                fontWeight={600}
+                style={{ opacity: collapsed ? 0 : 0.7, letterSpacing: '0.5px' }}
+              >
+                Extra
+              </Typography>
+            </div>
+
+            <Menu>
+              <MenuItem icon={<Book />}>Documentation</MenuItem>
+              <MenuItem disabled icon={<Service />}>
+                Examples
+              </MenuItem>
+            </Menu>
+          </div>
+        </div>
+      </Sidebar>
+    </div>
     <main className="min-h-screen max">
+        <button className="sb-button" onClick={() => setCollapsed(!collapsed)}>
+          Toggle
+        </button>
       <div className='flex flex-col items-center justify-center'>
         <div>
           Here is the dashboard
@@ -33,6 +170,6 @@ export default function Home() {
         </div>
       </div>
     </main>
-  </>
+  </div>
   )
 }
--- cyber-warden/src/components/CustomSidebar.tsx
+++ cyber-warden/src/components/CustomSidebar.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { Sidebar } from "./Sidebar";
 // import "react-pro-sidebar/dist/css/styles.css";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import SidebarItem from "./SidebarItem";
 // icon import
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import { Diamond } from '../icons/Diamond';
+import { BarChart } from '../icons/BarChart';
+import { Global } from '../icons/Global';
+import { InkBottle } from '../icons/InkBottle';
+import { Book } from '../icons/Book';
+import { Calendar } from '../icons/Calendar';
+import { ShoppingCart } from '../icons/ShoppingCart';
+import { Service } from '../icons/Service';
+
 
 
 
@@ -26,129 +34,118 @@ interface SidebarProps {
 
 
 const CustomSidebar: React.FC<SidebarProps> = ({ menuItems, currentPage }) => {
-    const [isCollapsed, setIsCollapsed] = useState(true);
-    // currentPage is a string that is used to check which page the user is currently viewing
-    // depending on that the corresponding icon will be blue
-    const [selected, setSelected] = useState({currentPage});
-    const [isMounted, setIsMounted] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+  const [toggled, setToggled] = useState(false);
+  const [broken, setBroken] = useState(false);
+  const [rtl, setRtl] = useState(false);
+  const [hasImage, setHasImage] = useState(false);
 
-    useEffect(() => {
-      setIsMounted(true);
+  // handle on RTL change event
+  const handleRTLChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRtl(e.target.checked);
+  };
+
+  // handle on image change event
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHasImage(e.target.checked);
+  };
+
+  
+
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  // currentPage is a string that is used to check which page the user is currently viewing
+  // depending on that the corresponding icon will be blue
+  const [selected, setSelected] = useState({currentPage});
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
     }, []);
+
     
     return (
-        <Box
-            sx={{
-                "& .Sidebar-inner": {
-                    background: `${"#e1e2fe"} !important`
-                },
-                "& .Sidebar-icon-wrapper": {
-                    backgroundColor: "transparent !important"
-                },
-                "& .Sidebar-inner-item": {
-                    padding: "5px 35px 5px 20 px !important"
-                },
-                "& .Sidebar-inner-item:hover": {
-                    color: "#868dfb !important"
-                },
-                "& .Sidebar-menu-item.active":{
-                    color: "#6870fa !important"
-                },
-                "& .Sidebar":{
-                    height: "100vh !important",
-                    position: "fixed !important",
-                    top: "0 !important",
-                    bottom: "0 !important"
-                },
-                transition: isCollapsed ? "padding-right 0.15s ease-in-out" : "padding-right 0.3s ease-in-out",
-                paddingRight: isCollapsed ? "80px" : "250px",
-            }}
-        >
-        <Sidebar collapsed={isCollapsed}>
-        <Menu>
-          {/* MENU ICON */}
-          <MenuItem
-            // when collapsed 
-            onClick={() => setIsCollapsed(!isCollapsed)}
-            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-            style={{
-              margin: "5px 0 0 0",
-              // color: "#3e4396",
-            }}
-          >
-            {/* when not collapsed */}
-            {!isCollapsed && (
-              <Box
-                display="flex"
-                alignItems="center"
-                ml="15px"
+      <div style={{ display: 'flex', height: '100vh' }}>
+      <Sidebar
+        collapsed={collapsed}
+        toggled={toggled}
+        onBackdropClick={() => setToggled(false)}
+        onBreakPoint={setBroken}
+        rtl={rtl}
+        breakPoint="md"
+        backgroundColor={'#ffffff'}
+        rootStyles={{
+          color: '#607489',
+        }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+          <div style={{ flex: 1, marginBottom: '32px' }}>
+            <div style={{ padding: '0 24px', marginBottom: '8px' }}>
+              <Typography
+                variant="body2"
+                fontWeight={600}
+                style={{ opacity: collapsed ? 0 : 0.7, letterSpacing: '0.5px' }}
               >
-                <IconButton style={{color: "#3e4396"}} onClick={() => setIsCollapsed(!isCollapsed)}>
-                  <MenuOutlinedIcon />
-                </IconButton>
-              </Box>
-            )}
-          </MenuItem>
+                General
+              </Typography>
+            </div>
+            <Menu >
+              <SubMenu
+                label="Charts"
+                icon={<BarChart />}
+              >
+                <MenuItem> Pie charts</MenuItem>
+                <MenuItem> Line charts</MenuItem>
+                <MenuItem> Bar charts</MenuItem>
+              </SubMenu>
+              <SubMenu label="Maps" icon={<Global />}>
+                <MenuItem> Google maps</MenuItem>
+                <MenuItem> Open street maps</MenuItem>
+              </SubMenu>
+              <SubMenu label="Theme" icon={<InkBottle />}>
+                <MenuItem> Dark</MenuItem>
+                <MenuItem> Light</MenuItem>
+              </SubMenu>
+              <SubMenu label="Components" icon={<Diamond />}>
+                <MenuItem> Grid</MenuItem>
+                <MenuItem> Layout</MenuItem>
+                <SubMenu label="Forms">
+                  <MenuItem> Input</MenuItem>
+                  <MenuItem> Select</MenuItem>
+                  <SubMenu label="More">
+                    <MenuItem> CheckBox</MenuItem>
+                    <MenuItem> Radio</MenuItem>
+                  </SubMenu>
+                </SubMenu>
+              </SubMenu>
+              <SubMenu label="E-commerce" icon={<ShoppingCart />}>
+                <MenuItem> Product</MenuItem>
+                <MenuItem> Orders</MenuItem>
+                <MenuItem> Credit card</MenuItem>
+              </SubMenu>
+            </Menu>
 
-          {!isCollapsed && (
-            <Box>
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <img
-                  alt="profile-user"
-                  width="100px"
-                  height="100px"
-                  src='logo.png'
-                  style={{ cursor: "pointer" }}
-                />
-              </Box>
-              <Box textAlign="center">
-                <Typography 
-                  variant="h3" 
-                  color={"#3e4396"} 
-                  m="10px 0 0 0"
-                  >
-                  Serial Scripter
-                </Typography>
+            <div style={{ padding: '0 24px', marginBottom: '8px', marginTop: '32px' }}>
+              <Typography
+                variant="body2"
+                fontWeight={600}
+                style={{ opacity: collapsed ? 0 : 0.7, letterSpacing: '0.5px' }}
+              >
+                Extra
+              </Typography>
+            </div>
 
-                <Typography
-                  variant="h4"
-                  color={"#2e7c67"}
-                  fontWeight="bold"
-                  sx={{ m: "10px 0 0 0" }}
-                >
-                  Keyboard Cowboys
-                </Typography>
-              </Box>
-            </Box>
-          )}
-
-          {/* <Box paddingLeft={isCollapsed ? undefined : "10%"} paddingTop={isCollapsed ? undefined : "30px"}> */}
-          <Box paddingTop={!isCollapsed && isMounted ? "30px" : undefined}>
-
-              <SidebarItem
-                title="Dashboard"
-                to="/"
-                icon={<HomeOutlinedIcon />}
-                selected={selected}
-                color={"#3e4396"}
-              />
-              <Box>
-              {/* Generate menu items dynamically if menuItems are passed*/}
-              {menuItems ? menuItems.map((item) => (
-                <SidebarItem
-                  key={item.title}
-                  title={item.title}
-                  to={item.to}
-                  icon={item.icon}
-                  selected={selected}
-                  color={"#3e4396"}
-                />
-              )) : null}
-              </Box>
-          </Box>
-        </Menu>
-        </Sidebar>
-        </Box>
+            <Menu>
+              <MenuItem icon={<Calendar />}>
+                Calendar
+              </MenuItem>
+              <MenuItem icon={<Book />}>Documentation</MenuItem>
+              <MenuItem disabled icon={<Service />}>
+                Examples
+              </MenuItem>
+            </Menu>
+          </div>
+        </div>
+      </Sidebar>
+      </div>
     );
 };
 
