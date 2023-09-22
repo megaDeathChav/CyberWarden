e8d5a5b5824dcc531be0fec6e14148ca242d63ae
Ana Sordo <007579995@coyote.csusb.edu>
2023-09-21 20:52:44 -0700

    Upate ipManagement.js file by  appending blacklist/whitelist ip's

--- api/routes/ipManagement.js
+++ api/routes/ipManagement.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const port = 3000;
 
 let blacklist = [];
 let whitelist = [];
 
-router.post('/blacklist/get', (req, res, next) => {
-  res.json({ ping: 'pong' });
+router.get('/blacklist/get', (req, res, next) => {
+  //this is for debugging
+  console.log('GET /blacklist/get route reached');
+  res.json({ blacklist });
+
 });
 
-router.get('/blacklist/add', (req, res, next) => {
-  res.json({ ping: 'pong' });
+router.post('/blacklist/add', (req, res, next) => {
+  const { ipAddress } = req.body;
+
+  blacklist.push(ipAddress);
+//this is for debugging
+  res.status(201).json({ message: 'IP address added to blacklist successfully' });
 });
 
-router.post('/whitelist/get', (req, res, next) => {
-  res.json({ ping: 'pong' });
+router.get('/whitelist/get', (req, res, next) => {
+  res.json({ whitelist });
 });
 
-router.get('/whitelist/add', (req, res, next) => {
-  res.json({ ping: 'pong' });
+router.post('/whitelist/add', (req, res, next) => {
+  const { ipAddress } = req.body;
+
+  whitelist.push(ipAddress);
+//this is for debugging
+  res.status(201).json({ message: 'IP address added to whitelist successfully' });
 });
 
 module.exports = router;
