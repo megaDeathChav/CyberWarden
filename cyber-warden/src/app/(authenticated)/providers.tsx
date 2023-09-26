973bd79a83258224d0c2b062b81ec56bc2ef21fa
PR1Z4R4K1 <jaylonignacio@gmail.com>
2023-09-26 10:20:04 -0700

    suppressHydrationError...

--- cyber-warden/src/app/(authenticated)/providers.tsx
+++ cyber-warden/src/app/(authenticated)/providers.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 export default function Providers({ children }: { children: React.ReactNode})
 {
     return (
+        
         <NextUIProvider>
             <NextThemesProvider
                 attribute="class"
