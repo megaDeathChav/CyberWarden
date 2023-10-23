8b5c26655ec025e04f56075a423b8239b95eee59
xKevBac <kbach829@gmail.com>
2023-10-23 11:20:00 -0700

    Remove dropdown on Sidebar,trying make it buttons

--- cyber-warden/src/components/Sidebar.tsx
+++ cyber-warden/src/components/Sidebar.tsx
@@ -54,86 +54,25 @@ export function SidebarWithLogo() {
         </Typography>
       </div>
       <List>
-        <Accordion
-          open={open === 1}
-          icon={
-            <ChevronDownIcon
-              strokeWidth={2.5}
-              className={`mx-auto h-4 w-4 transition-transform text-black dark:text-gray-200 ${open === 1 ? "rotate-180" : ""}`}
-            />
-          }
-        >
-          <ListItem className="p-0 hover:bg-blue-300 hover:dark:bg-purple-900">
-            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3">
-              <ListItemPrefix>
-                <PresentationChartBarIcon className="h-5 w-5 text-blue-800 dark:text-gray-400 " />
-              </ListItemPrefix>
-              <Typography color="blue-gray" className="mr-auto font-normal text-black dark:text-gray-200">
-                Dashboard
-              </Typography>
-            </AccordionHeader>
-          </ListItem>
-          <AccordionBody className="py-1">
-            <List className="p-0">
-              <ListItem className="text-black hover:bg-blue-300 dark:text-gray-200 hover:dark:bg-purple-900">
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5 text-black dark:text-gray-200"/>
-                </ListItemPrefix>
-                Analytics
-              </ListItem>
-              <ListItem className='text-black hover:bg-blue-300 dark:text-gray-200 hover:dark:bg-purple-900'>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5 text-black dark:text-gray-200" />
-                </ListItemPrefix>
-                Reporting
-              </ListItem>
-              <ListItem className='text-black hover:bg-blue-300 dark:text-gray-200 hover:dark:bg-purple-900'>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5 text-black dark:text-gray-200" />
-                </ListItemPrefix>
-                Projects
-              </ListItem>
-            </List>
-          </AccordionBody>
-        </Accordion>
-        <Accordion
-          open={open === 2}
-          icon={
-            <ChevronDownIcon
-              strokeWidth={2.5}
-              className={`mx-auto h-4 w-4 transition-transform text-black dark:text-gray-200 ${open === 2 ? "rotate-180" : ""}`}
-            />
-          }
-        >
-          <ListItem className="p-0  hover:bg-blue-300 hover:dark:bg-purple-900">
-            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
-              <ListItemPrefix>
-                <NewspaperIcon className="h-5 w-5 text-blue-800 dark:text-gray-400 " />
-              </ListItemPrefix>
-              <Typography color="blue-gray" className="mr-auto font-normal text-black dark:text-gray-200">
-                Server Logs
-              </Typography>
-            </AccordionHeader>
-          </ListItem>
-          <AccordionBody className="py-1">
-            <List className="p-0">
-              <ListItem className='text-black hover:bg-blue-300 dark:text-gray-200 hover:dark:bg-purple-900'>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5 text-black dark:text-gray-200" />
-                </ListItemPrefix>
-                Orders
-              </ListItem>
-              <ListItem className='text-black hover:bg-blue-300 dark:text-gray-200 hover:dark:bg-purple-900'>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5 text-black dark:text-gray-200" />
-                </ListItemPrefix>
-                Products
-              </ListItem>
-            </List>
-          </AccordionBody>
-        </Accordion>
-        <hr className="my-2 border-blue-gray-50" />
+        <button className="p-0 hover:bg-blue-300 hover:dark:bg-purple-900 w-full text-left">
+        <div className="flex items-center">
+          <PresentationChartBarIcon className="h-5 w-5 text-blue-800 dark:text-gray-400" />
+          <span className="ml-2 font-normal text-black dark:text-gray-200">
+              Dashboard
+         </span>
+        </div>
+        </button>
+
+        <button className="p-0 hover:bg-blue-300 hover:dark:bg-purple-900 w-full text-left">
+        <div className="flex items-center">
+          <NewspaperIcon className="h-5 w-5 text-blue-800 dark:text-gray-400" />
+          <span className="ml-2 font-normal text-black dark:text-gray-200">
+            Server Logs
+          </span>
+       </div>
+        </button>
       </List>
+      
       <List className="flex justify-end h-full mb-6">
         <ListItem className='text-black hover:bg-blue-300 dark:text-gray-200 hover:dark:bg-purple-900'>
           <ThemeSwitcher />
